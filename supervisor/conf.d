0a7715020e3bf79cc579ba900091afe2d9ff7741
MohieEldinMuhammad <89604782+MohieEldinMuhammad@users.noreply.github.com>
2021-09-24 22:43:46 +0200

    Create conf.d

---
+++ supervisor/conf.d
@@ -0,0 +1,7 @@
+[program:hello_world]
+directory=/home/ec2-user/users/hello_world
+command=/home/ec2-user/users/.env/bin/gunicorn app:app -b localhost:8000
+autostart=true
+autorestart=true
+stderr_logfile=/var/log/hello_world/hello_world.err.log
+stdout_logfile=/var/log/hello_world/hello_world.out.log
