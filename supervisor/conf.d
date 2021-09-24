589cd42bd461b711ddc0f3bba051ee09eb08dee9
MohieEldinMuhammad <89604782+MohieEldinMuhammad@users.noreply.github.com>
2021-09-24 23:01:57 +0200

    Update conf.d

--- supervisor/conf.d
+++ supervisor/conf.d
@@ -1,6 +1,6 @@
 [program:hello_world]
 directory=/home/ec2-user/users/hello_world
-command=/home/ec2-user/users/.env/bin/gunicorn app:app -b localhost:8000
+command=/home/ec2-user/users/.env/bin/gunicorn flask_app:app -b localhost:8000
 autostart=true
 autorestart=true
 stderr_logfile=/var/log/hello_world/hello_world.err.log
