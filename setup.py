1b44152b3fff0c0c07a6d91d1e7a4b00d206c4c0
MohieEldinMuhammad <89604782+MohieEldinMuhammad@users.noreply.github.com>
2021-09-26 20:20:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 setup(
-   name='mvse',
+   name='backend_demo',
    version='0.0.1',
    description='backend demo',
    author='',
    author_email='no-one@ogoul.com',
-   packages=['mvse'],
+   packages=['backend_demo'],
    install_requires=[
        'flask',
-      'gunicorn',
-     'nginx',
-     'supervisor'
+       'gunicorn',
+       'nginx',
+       'supervisor'
    ]
 )
